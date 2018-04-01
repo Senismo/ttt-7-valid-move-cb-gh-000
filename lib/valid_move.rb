2f3d9292d2803ef74d8438a82962c5c98097edd9
Salak Sen <benismail31@gmail.com>
2018-04-01 19:28:14 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index >= 0 && index <= 8 && position_taken?(board, position)
-    return true
+  if position_taken?(board, position)
+    return false
   else
     return false
   end
